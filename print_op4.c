afb3f8a94c12d358f3a17d295b6d47554f42fd5f
DGSellin <69220067+DGSellin@users.noreply.github.com>
2021-11-21 18:52:17 -0300

    Add files via upload

---
+++ print_op4.c
@@ -0,0 +1,16 @@
+void main(){
+    int num;
+    printf("                     =                                                            Opcao Selecionada: Dar um Lance                                                                    =\n");
+    printf("                     =---------------------------------------------------------------------------------------------------------------------------------------------------------------=\n");
+    printf("                     =                                                                  Entre com seu Nome:                                                                          =\n");
+    printf("                                                                                               R:");
+    scanf("%d", num);
+    printf("                     =                                                              Entre com o Valor do Lance:                                                                      =\n");
+    printf("                                                                                               R:");
+    scanf("%d", num);
+    printf("                     =                                                              Entre com o Nome do Produto:                                                                     =\n");
+    printf("                                                                                               R:");
+    scanf("%d", num);
+    printf("                     =                                                              Lance realizado com Sucesso!                                                                     =\n");
+    printf("                     =================================================================================================================================================================\n");
+}
